6df46063d4414cf8163e459103b2fd6062196bd6
funtikova <funtikova95@mail.ru>
2024-02-03 22:20:04 +0300

    Добавлены классы для конфигурации

---
+++ src/main/java/urfu/picnic/config/MvcConfig.java
@@ -0,0 +1,12 @@
+package urfu.picnic.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+
+@Configuration
+public class MvcConfig {
+
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/").setViewName("index");
+    }
+}
